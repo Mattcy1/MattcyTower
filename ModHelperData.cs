91e8f257f8475323f4cf3b39b55277a424063fd5
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-08-07 14:26:16 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,17 @@
+﻿using BTD_Mod_Helper;
+using MattcyTower;
+using MelonLoader;
+
+        public static class ModHelperData
+        {
+            public const string WorksOnVersion = "37.0";
+            public const string Version = "1.1";
+            public const string Name = "Mattcy Tower";
+            public const string Description =
+                "Adds in a new custom tower themed around me Also Check Out My Youtube Channel Its MattcyYT with the litte orange and blue icon thanks!";
+
+            public const string RepoOwner = "Mattcy1";
+            public const string RepoName = "Mattcy-tower";
+            public const string Icon = "MattcyTower-Icon.png";
+        }
+    
